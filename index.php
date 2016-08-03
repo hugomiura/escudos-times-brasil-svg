12979d7313ee9b1363caccc5b5f78506d6a07109
Hugo Miura <hugo.miura@hotmail.com>
2016-08-02 23:31:46 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Escudos dos Times Brasileiros</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <!--#include file="serie-a.svg"-->
+	<h2>Série A</h2>
+	<ul>
+	<li>	
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#america-mg"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#atletico-mg"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#atletico-pr"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#botafogo"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#chapecoense"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#corinthians"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#coritiba"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#cruzeiro"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#figueirense"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#flamengo"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#fluminense"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#gremio"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#internacional"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#palmeiras"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ponte-preta"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#santa-cruz"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#santos"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sao-paulo"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sport"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vitoria"></use>
+		</svg>
+	</li>
+</ul>
+
+<!--#include file="serie-b.svg"-->
+<h2>Série B</h2>
+
+<ul>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#atletico-go"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#avai"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#bahia"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#bragantino"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#brasil-de-pelotas"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ceara"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#crb"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#criciuma"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#goias"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#joinville"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#londrina"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#luverdense"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#nautico"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#oeste"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#parana"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#paysandu"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sampaio-correa"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#tupi"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vasco"></use>
+		</svg>
+	</li>
+	<li>
+		<svg class="escudo">
+			<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vila-nova"></use>
+		</svg>
+	</li>
+</ul>
+
+</body>
+</html>
